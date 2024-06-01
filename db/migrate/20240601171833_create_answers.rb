975b836e6b45c18d413503bd3195bf1286a1b2d3
kgaleev <kkgaleev@gmail.com>
2024-06-01 23:02:39 +0500

    feat: limit NULL values in tables

--- db/migrate/20240601171833_create_answers.rb
+++ db/migrate/20240601171833_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.string :body
-      t.boolean :correct
+      t.boolean :correct, null: false
 
       t.timestamps
     end
