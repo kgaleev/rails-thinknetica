28e1c2eedb039b8457ce2a526714683388450875
kgaleev <kkgaleev@gmail.com>
2025-06-18 19:20:29 +0500

    fix: add missing Result model, change list method in User model to list tests instead of results

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def list_tests(level)
-    Result.joins(:test)
-          .where(tests: { level: }, user_id: id)
+    Test.joins("JOIN results ON results.test_id = tests.id")
+        .where('results.user_id = ? AND tests.level = ?', id, level)
   end
 end
