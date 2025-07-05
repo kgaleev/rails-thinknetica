997d0b5f2c4e93ab056c4a9cbb8bb264afd5b4e2
kgaleev <kkgaleev@gmail.com>
2025-07-05 17:44:00 +0500

    feat: add associations to tables, add model tests_user, add join table tests_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :tests_users
+  has_many :tests, through: :tests_users
   def list_tests(level)
     Test.joins("JOIN results ON results.test_id = tests.id")
         .where('results.user_id = ? AND tests.level = ?', id, level)
